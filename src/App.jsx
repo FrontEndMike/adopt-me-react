a987cf5c159c3c7ad63d47921f2e2e9da5a97e5b
Mike Jackson <mike@airtightdesign.com>
2024-08-15 00:46:17 -0500

    dev env setup

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Pitbull",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Terry",
+      breed: "Maine Coon",
+    }),
+    React.createElement(Pet, {
+      animal: "Tiger",
+      name: "Mike",
+      breed: "Bengal",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
