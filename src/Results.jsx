ed5c466deac060900e259ebf6afce2271061ed10
Mike Jackson <mike@airtightdesign.com>
2024-08-30 18:01:09 -0400

    Build Results component

---
+++ src/Results.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+            breed={pet.breed}
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Pet from "./Pet";
 import { useState, useEffect } from "react";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "reptile"];
 
@@ -78,14 +78,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
