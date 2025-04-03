8a9042cb7ca3179e1e0e73f9427cd6ee5fcfc817
Mike Jackson <mike@airtightdesign.com>
2025-04-03 18:56:14 -0400

    restyle details page with tailwind

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const Carousel = ({
-  images = ["http://pets-images.dev.apis.com/pets/none.jpg"],
+  images = ['http://pets-images.dev.apis.com/pets/none.jpg'],
 }) => {
-  const [active, setActive] = useState(0);
+  const [active, setActive] = useState(0)
 
   const handleIndexClick = (e) => {
-    setActive(+e.target.dataset.index);
-  };
+    setActive(+e.target.dataset.index)
+  }
 
   return (
-    <div className="carousel">
-      <img src={images[active]} alt="animal hero" />
-      <div className="carousel-smaller">
+    <div className="mb-4 grid grid-flow-col grid-cols-1 justify-between md:grid-cols-2">
+      <img src={images[active]} className="rounded-full" alt="animal hero" />
+      <div className="carousel-smaller text-center">
         {images.map((photo, index) => (
           // eslint-disable-next-line
           <img
@@ -20,13 +20,13 @@ const Carousel = ({
             data-index={index}
             key={photo}
             src={photo}
-            className={index === active ? "active" : ""}
+            className={index === active ? 'active' : ''}
             alt="animal thumbnail"
           />
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Carousel;
+export default Carousel
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,75 +1,84 @@
-import React from "react";
-import { useState, useContext } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import AdoptedPetContext from "./AdoptedPetContext";
-import fetchPet from "./fetchPet";
-import Carousel from "./Carousel";
-import ErrorBoundary from "./ErrorBoundary";
-import Loader from "./Loader";
-import Modal from "./Modal";
+import React from 'react'
+import { useState, useContext } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import AdoptedPetContext from './AdoptedPetContext'
+import fetchPet from './fetchPet'
+import Carousel from './Carousel'
+import ErrorBoundary from './ErrorBoundary'
+import Loader from './Loader'
+import Modal from './Modal'
 
 const Details = () => {
-  const [showModal, setShowModal] = useState(false);
-  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false)
+  const navigate = useNavigate()
 
   // eslint-disable-next-line no-unused-vars
-  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
-  const { id } = useParams();
-  const results = useQuery(["details", id], fetchPet);
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext)
+  const { id } = useParams()
+  const results = useQuery(['details', id], fetchPet)
 
   if (results.isLoading) {
-    console.log("loading");
+    console.log('loading')
     return (
       <div className="loading-pane">
         <Loader />
       </div>
-    );
+    )
   }
 
-  const pet = results.data.pets[0];
+  const pet = results.data.pets[0]
 
   return (
-    <div className="details">
-      <Carousel images={pet.images} />
-      <div>
-        <h1>{pet.name}</h1>
-        <h2>{pet.animal}</h2>
-        <h2>{pet.breed}</h2>
-        <h2>
-          {pet.city}, {pet.state}
-        </h2>
-        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
-        <p>{pet.description}</p>
-        {showModal ? (
-          <Modal>
-            <div>
-              <h1>Would you like to adopt {pet.name}?</h1>
-              <div className="buttons">
-                <button
-                  onClick={() => {
-                    setAdoptedPet(pet);
-                    navigate("/");
-                  }}
-                >
-                  Yes
-                </button>
-                <button onClick={() => setShowModal(false)}>No</button>
+    <div className="details p-4">
+      <div className="mx-auto mb-8 max-w-[768px] rounded-2xl border-[1px] border-black bg-white p-4 shadow-lg">
+        <Carousel images={pet.images} />
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-3xl">{pet.name}</h1>
+          <h2 className="capitalize">{pet.animal}</h2>
+          <h2>{pet.breed}</h2>
+          <h2>
+            {pet.city}, {pet.state}
+          </h2>
+          <p className="">{pet.description}</p>
+          <div className="my-4">
+            <button
+              className="button mx-auto"
+              onClick={() => setShowModal(true)}
+            >
+              Adopt {pet.name}
+            </button>
+          </div>
+          {showModal ? (
+            <Modal>
+              <div className="w-full max-w-[400px] p-4">
+                <h3 className="mb-4">Would you like to adopt {pet.name}?</h3>
+                <div className="buttons">
+                  <button
+                    onClick={() => {
+                      setAdoptedPet(pet)
+                      navigate('/')
+                    }}
+                  >
+                    Yes
+                  </button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
               </div>
-            </div>
-          </Modal>
-        ) : null}
+            </Modal>
+          ) : null}
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 function DetailsErrorBoundary(props) {
   return (
     <ErrorBoundary>
       <Details {...props} />
     </ErrorBoundary>
-  );
+  )
 }
 
-export default DetailsErrorBoundary;
+export default DetailsErrorBoundary
