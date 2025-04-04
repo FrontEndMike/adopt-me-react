7522f4736abbcd03b50a69bb00ed336f3e944863
Mike Jackson <mike@airtightdesign.com>
2025-04-04 16:34:42 -0400

    complete server logic

--- src/ClientApp.jsx
+++ src/ClientApp.jsx
@@ -1,5 +1,6 @@
 import { hydrateRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
 import App from './App'
 
 hydrateRoot(
--- src/ServerApp.jsx
+++ src/ServerApp.jsx
@@ -1,5 +1,7 @@
 import { renderToPipeableStream } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom/server'
+import React from 'react'
+
 import App from './App'
 
 export default function render(url, opts) {
